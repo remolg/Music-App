c523c8b5ce62ae941b22536a7ce800c02a33b30a
Remolg <sadikahmet25@gmail.com>
2023-10-07 16:24:10 +0300

    Navbar Ended

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,32 @@
-const Searchbar = () => (
-  <div>Loader</div>
-);
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Paper, IconButton } from '@mui/material'
+import { Search } from '@mui/icons-material'
 
-export default Searchbar;
+const SearchBar = () => {
+    return (
+        <Paper
+            component="form"
+            onSubmit={() => { }}
+            sx={{
+                borderRadius: 20,
+                border: '1px solid #e3e3e3',
+                pl: 2,
+                boxShadow: 'none',
+                mr: { sm: 5 }
+            }}
+        >
+            <input
+                className='search-bar'
+                placeholder='Search...'
+                value=""
+                onChange={() => { }}
+            />
+            <IconButton type='submit' sx={{ p: '10px', color: 'red' }} >
+                <Search />
+            </IconButton>
+        </Paper>
+    )
+}
+
+export default SearchBar
--- src/components/index.js
+++ src/components/index.js
@@ -1,25 +1,6 @@
-import Sidebar from './Sidebar';
-import Searchbar from './Searchbar';
-import SongCard from './SongCard';
-import TopPlay from './TopPlay';
-import ArtistCard from './ArtistCard';
-import DetailsHeader from './DetailsHeader';
-import SongBar from './SongBar';
-import RelatedSongs from './RelatedSongs';
-import MusicPlayer from './MusicPlayer';
-import Loader from './Loader';
-import Error from './Error';
-
-export {
-  TopPlay,
-  Sidebar,
-  SongCard,
-  Searchbar,
-  ArtistCard,
-  DetailsHeader,
-  SongBar,
-  RelatedSongs,
-  MusicPlayer,
-  Loader,
-  Error,
-};
+export { default as Navbar } from './Navbar';
+export { default as Feed } from './Feed';
+export { default as ChannelDetail } from './ChannelDetail';
+export { default as SearchFeed } from './SearchFeed';
+export { default as VideoDetail } from './VideoDetail';
+export { default as SearchBar } from './SearchBar';
