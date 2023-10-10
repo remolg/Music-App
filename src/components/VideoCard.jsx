4d83146a688d39e6749882951084420c0b322299
Remolg <sadikahmet25@gmail.com>
2023-10-10 16:11:13 +0300

    the channel card is set , videos have been added to the main page

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -17,7 +17,7 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
             <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }} >
                 <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
                     <Typography variant="subtitle1" fontWeight='bold' color="#fff">
-                        {snippet?.title.slice(0.60) || demoVideoTitle.slice(0, 60)}
+                        {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
                     </Typography>
                 </Link>
                 <Link to={snippet?.chanelId ? `/channel/${snippet?.chanelId}` : demoChannelUrl}>
