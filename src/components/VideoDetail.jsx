d82efd29cfb700752c115a4bd6352a083eb97773
Remolg <sadikahmet25@gmail.com>
2023-10-10 22:22:20 +0300

    Interface and cards are finished, last update before the end

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -3,15 +3,18 @@ import { Box, Stack, Typography } from "@mui/material"
 import { useEffect, useState } from "react"
 import ReactPlayer from "react-player"
 import { Link, useParams } from "react-router-dom"
-import { Video } from './';
+import { Videos } from './';
 import { fetchFromAPI } from "../utils/fetchFromAPI"
 
 
 const VideoDetail = () => {
     const [videoDetail, setVideoDetail] = useState(null)
+    const [videos, setVideos] = useState(null)
     const { id } = useParams();
     useEffect(() => {
         fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) => setVideoDetail(data.items[0]));
+
+        fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then((data) => setVideos(data.items))
     }, [id])
 
     if (!videoDetail?.snippet) return 'Loading...'
@@ -24,12 +27,12 @@ const VideoDetail = () => {
                     <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
                         <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`}
                             className='react-player' controls />
-                        <Typography color='#fff' variant="h5" fontWeight='bold'>
+                        <Typography color='#fff' variant="h5" fontWeight='bold' p={2}>
                             {title}
                         </Typography>
                         <Stack direction='row' justifyContent='space-between' sx={{ color: '#fff' }} py={1} px={2}>
                             <Link to={`/channel/${channelId}`}>
-                                <Typography variant={{ sm: 'subtitle1', md: 'h6' }} color='#fff' >
+                                <Typography variant={{ sm: 'subtitle1', md: 'h6' }} color='#fff' justifyContent='center'>
                                     {channelTitle}
                                     <CheckCircle sx={{ fontSize: '12px', color: 'gray', ml: '5px' }} />
                                 </Typography>
@@ -44,6 +47,9 @@ const VideoDetail = () => {
                             </Stack>
                         </Stack>
                     </Box>
+                </Box>
+                <Box px={2} py={{ md: 1, xs: 5 }} justifyContent='center' alignItems='center'>
+                    <Videos videos={videos} direction='column' />
                 </Box>
             </Stack >
         </Box >
