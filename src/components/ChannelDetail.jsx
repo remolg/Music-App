c6d57a494f9cee947b51e9e7e147a68af3e1cabf
Remolg <sadikahmet25@gmail.com>
2023-10-10 18:12:27 +0300

    ChannelCard and ChannelDetail edited

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import { Box } from "@mui/material"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { ChannelCard, Videos } from "."
+import { fetchFromAPI } from "../utils/fetchFromAPI"
+
 
 const ChannelDetail = () => {
+    const [channelDetail, setChannelDetail] = useState(null)
+    const [videos, setVideos] = useState([])
+
+    const { id } = useParams();
+
+    console.log(channelDetail, videos)
+
+    useEffect(() => {
+        fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]));
+
+        fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) => setVideos(data?.items));
+    }, [id])
+
     return (
-        <div>ChannelDetail</div>
+        <Box minHeight="95vh">
+            <Box>
+                <div style={{
+                    background: 'linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(4,11,213,1) 100%)',
+                    zIndex: 10,
+                    height: '300px'
+                }}
+                />
+                <ChannelCard channelDetail={channelDetail} marginTop='-110px' />
+            </Box>
+        </Box>
     )
 }
 
